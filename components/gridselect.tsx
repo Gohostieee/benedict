46ce8848bb9d11769d93ca2033849081aff9322d
Gohostieee <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>
2022-09-14 01:22:13 -0400

    initial commit

---
+++ components/gridselect.tsx
@@ -0,0 +1,52 @@
+import { gridSelData } from "../interface";
+import type { NextPage } from 'next'
+
+
+type UserType = {
+    dob: string,
+    firstName: string,
+    userBio: string,
+  };
+interface UserProps {
+    user: UserType,
+  }
+interface props {
+    data:string
+}
+
+function parseData(data:gridSelData[]):JSX.Element[] {
+	let result:JSX.Element[] = [];
+	data.forEach(x => {
+		result.push(
+		<div className="">
+            <div className="m-auto text-center ">
+            <a href={x.href}><p className=" border inline p pr-2 pl-2 text-2xl text-white font-bold hover:bg-white hover:text-black transition-all  ">{x.name.toUpperCase()}</p></a>
+
+            </div>
+                <div className="border-2 mt-4 border-white p-6 m-auto overflow-hidden w-[300px] h-[300px] transition-all bg-opacity-70 bg-black hover:p-12 hover:bg-opacity-0 hover:border-white">
+			        <a href={x.href} className = "inline w-[100%] h-[100%]">
+				    
+                        <img src = {x.img.src} className = "h-[100%]"/>
+                    </a>
+			    </div>
+		</div>
+        )
+	})
+	return result
+	
+}
+function GridSelect({data}:props)   {
+    console.log(data,"what")
+    
+    const propsJson:gridSelData[] = JSON.parse(data)
+    console.log(propsJson)
+    return (
+        <div className="h-[100%] w-[100%] grid grid-cols-3 grid-rows-3">
+            {parseData(propsJson)}
+        </div>
+    )
+
+}
+
+
+export default  GridSelect
---
+++ interface/index.ts
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image"
+
+
+export interface gridSelData {
+   
+        img:StaticImageData,
+        name: string,
+        href:string
+    
+}
+
+export interface img {
+        src:string,
+        width:Number,
+        height:Number
+}
